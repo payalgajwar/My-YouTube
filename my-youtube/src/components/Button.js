a2f73b90f347d6b14d8eb70a2d7c179a650d3739
payalgajwar <payalgajwar9997@gmail.com>
2023-11-30 21:57:03 +0530

    youtube-4

--- my-youtube/src/components/Button.js
+++ my-youtube/src/components/Button.js
@@ -1,7 +1,9 @@
 import React from "react";
 
 const Button = ({ name }) => {
-  return <div>Button</div>;
+  return (
+    <button className="px-4 py-2 m-2 rounded-lg bg-gray-100">{name}</button>
+  );
 };
 
 export default Button;
--- my-youtube/src/components/ButtonList.js
+++ my-youtube/src/components/ButtonList.js
@@ -1,14 +1,23 @@
 import React from "react";
+import Button from "./Button";
 
 const ButtonList = () => {
   return (
     <div className="flex font-bold p-4">
-      <button className="px-4 py-2 bg-gray-400">All</button>
-      <button className="px-4 py-2 bg-gray-400">Music</button>
-      <button className="px-4 py-2 bg-gray-400">Tseries</button>
-      <button className="px-4 py-2 bg-gray-400">Bollywood Music</button>
-      <button className="px-4 py-2 bg-gray-400">Mantras</button>
-      <button className="px-4 py-2 bg-gray-400">Live</button>
+      <Button name="All" />
+      <Button name="Music" />
+      <Button name="Tseries" />
+      <Button name="Bollywood Music" />
+      <Button name="Mantras" />
+      <Button name="Live" />
+      <Button name="Cooking" />
+      <Button name="Cricket" />
+      <Button name="Soccer" />
+      <Button name="Gaming" />
+      <Button name="Comedy" />
+      <Button name="Songs" />
+      <Button name="Drama" />
+      <Button name="Cricket" />
     </div>
   );
 };
